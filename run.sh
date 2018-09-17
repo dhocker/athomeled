775b1d3bb98c3ba49507d9624ab4a095531ac129
Dave Hocker <athomex10@gmail.com>
2018-09-17 13:59:45 -0500

    Run a specif venv or the default

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-# Designed to be run sudo
-source /home/pi/Virtualenvs/athomeled/bin/activate
+echo Designed to be run sudo
+if [ -z "$1" ]; then
+	echo Using venv athomeled3
+	source /home/pi/Virtualenvs/athomeled3/bin/activate
+else
+	echo Using venv $1
+	source /home/pi/Virtualenvs/$1/bin/activate
+fi
 python at_home_led.py
